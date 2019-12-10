58bd52b41a4b62838652da42cfdb977925ff29cd
Srbnsng <serbysinge@gmail.com>
2019-12-10 13:36:07 +0200

    Spring project created!

---
+++ ArobsMeetup/ArobsMeetup/src/main/java/com/arobs/ArobsMeetup/ArobsMeetupApplication.java
@@ -0,0 +1,13 @@
+package com.arobs.ArobsMeetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArobsMeetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArobsMeetupApplication.class, args);
+	}
+
+}
